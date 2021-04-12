d3d8f9f8eb47c3d2072e29ada828d47fbc694648
EL BERGUI ADAM <46801088+XHAL9000@users.noreply.github.com>
2021-04-12 12:18:25 +0000

    Update tsne.py

--- tsne.py
+++ tsne.py
@@ -1,4 +1,3 @@
-
 def diff_ij(Y):
     """
     Tensor of  vector differences 
@@ -31,7 +30,8 @@ def _preplixity(mat_dist,var):
     Compute the preplexity given variances
     """
     mat_prob = prob_ij(mat_dist,var)
-    entropy = - mat_prob * np.log(mat_prob+0.001)  # add 0.001 because mat_prob[i,i] = 0
+    mat_prob = np.maximum(mat_prob,1e-12)
+    entropy = - mat_prob * np.log(mat_prob)
     entropy = np.sum(entropy,axis=1)
     return 2**entropy,mat_prob
 
@@ -45,8 +45,8 @@ def bisection_var(mat_dist,prep_wanted=30.0,tol = 0.01, max_iter=30):
     prep,mat_prob = _preplixity(mat_dist,var)
     diff_prep = prep - prep_wanted
     niter = 0
-    a = 0.001 * np.ones(n)  #Lower bound
-    b = (n*2)* np.ones(n)  #Upper bound
+    a = 1e-12 * np.ones(n)  #Lower bound
+    b = (2*n)* np.ones(n)  #Upper bound
     while np.linalg.norm( diff_prep ) > tol and  niter < max_iter:
         for i in range(n) : 
             if  diff_prep[i] <  0 :
@@ -62,38 +62,62 @@ def bisection_var(mat_dist,prep_wanted=30.0,tol = 0.01, max_iter=30):
 
 
 def KL_divergence(P,Q):
-    return np.sum( P * np.log(P/Q +0.001 ) )
+    return np.sum( P * np.log(P/Q) )
 
 def t_qij(q_dist):
     n = q_dist.shape[0]
     coef =  1 / (q_dist + 1)
-    coef = np.exp(coef)
     for i in range(n) :
         coef[i,i] = 0 # we set qii = 0 since we are only intrested in modeling pairwise similarities
     q_prob = coef / np.sum(coef)
-    return q_prob
+    return np.maximum(q_prob,1e-12)
 
 
 def gradient(p_prob,y_embed):
+    
     q_diff = diff_ij(y_embed)
     q_dist = np.sum(q_diff**2,axis=2)
     q_prob = t_qij(q_dist)
-    grad = np.dot(np.dot((p_prob - q_prob) , q_diff).T ,   (1 / (q_dist + 1) )) 
-    grad =  4 * np.sum(grad, axis=1)
-    return grad.T
+    grad = np.zeros_like(y_embed)
+    for i in range(y_embed.shape[0]):
+        for j in range(y_embed.shape[0]):
+            grad[i,:] += 4*(p_prob[i,j] - q_prob[i,j]) *  (1 / (q_dist[i,j] + 1) )  * q_diff[i,j,:]
+    return grad,q_prob
     
-def t_sne(X,n_dim=2,prep_wanted=10,lr= 0.1 , momentum = 0.5, epochs = 1000):
+def t_sne(X,n_dim=2,prep_wanted=30,lr= 0.5 , epochs = 1000):
     n = X.shape[0]
     p_diff = diff_ij(X)
     p_dist = np.sum(p_diff**2,axis=2) # NxN euclidian distances matrix
     prep,p_prob,var = bisection_var(p_dist,prep_wanted,tol = 0.001, max_iter=500)
     p_prob = (p_prob + p_prob.T) / (2 *n)
+    p_prob = 4 * p_prob
     y_embed = 0.0001 * np.random.randn(n,n_dim) # Generate random embeddings with 10E-4 std
     y_embed_old = y_embed
-    for i in range(epochs) : 
-        grad=  gradient(p_prob,y_embed)
+    for itr in range(epochs) : 
+        
+        grad,q_prob =  gradient(p_prob,y_embed)
+                
+        if itr < 20:
+            momentum = 0.5
+        else:
+            momentum = 0.8
+        
         y_embed_n =  y_embed + lr * grad + momentum * (y_embed - y_embed_old)   
         y_embed_old = y_embed
         y_embed = y_embed_n
+        
+        if (itr + 1) % 10 == 0:
+            C = KL_divergence(p_prob,q_prob)
+            print("Iteration ", (itr + 1), ": error is ", C)
+            if (itr+1) != 10:
+                ratio = C/oldC
+                lr = 1/(1+ratio)
+                print("ratio ", ratio)
+                if ratio >= 0.95:
+                    break
+            oldC = C
+        if itr == 100:
+            p_prob = p_prob / 4
+        
     
     return y_embed
